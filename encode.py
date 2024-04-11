cb16b723b7dfd7740434083b48ea4ef6bfe4e741
VeronicaR-UF <121985700+VeronicaR-UF@users.noreply.github.com>
2024-04-11 11:14:55 -0400

    This is the encode file

---
+++ encode.py
@@ -0,0 +1,35 @@
+def Menu():
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit\n')
+    return None
+
+
+action = 0
+password = '00000000'
+password2 = ''
+
+
+# if '__name__' == '__main__':
+Menu()
+while action != 3:
+    action = int(input('Please enter an option: '))
+    if action == 1:
+        password = (input('Please enter your password to encode: '))
+        print('Your password has been encoded and stored!')
+    elif action == 2:
+        for ii in range(0, len(password), 1):
+            if password[ii] == '7':
+                password2 += '0'
+            elif password[ii] == '8':
+                password2 += '1'
+            elif password[ii] == '9':
+                password2 += '2'
+            else:
+                password2 += str(int(password[ii]) + 3)
+        print(f'The encoded password is {password2}, and the original password is {password}')
+    elif action == 3:
+        action = 3
+    else:
+        print('Not a valid option.')
